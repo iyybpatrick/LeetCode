9bbed42e1afc16f4b711f668b7ac85cabef2ae9e
Yuebin Yang <patrick.ybyang@gmail.com>
2017-02-14 21:29:59 +0800

    Practice

--- JAVA/Linked List/_109ConvertSortedListtoBinarySearchTree.java
+++ JAVA/Linked List/_109ConvertSortedListtoBinarySearchTree.java
@@ -15,36 +15,22 @@ public class _109ConvertSortedListtoBinarySearchTree {
 		      TreeNode(int x) { val = x; }
 		  }
 		  public TreeNode sortedListToBST(ListNode head) {
-		        if(head==null) return null;
-		        TreeNode t_head=new TreeNode(head.val); head=head.next;
-		        while(head!=null){
-		        	
-		        	//smaller
-		        	if(head.val<t_head.val){
-		        		t_head.left=Tree_add(t_head.left,head.val);
-		        	}
-		        	//bigger
-		        	else{
-		        		System.out.println("heihei");
-		        		t_head.right=Tree_add(t_head.right,head.val);
-		        	}
-		        	head=head.next;
-		        }//while
-		        return t_head;
+		        if(head==null)  return null;
+		        return create_tree(head,null);
 		    }
-		  public TreeNode Tree_add(TreeNode sub_head,int value){
-			  if(sub_head==null){
-				 sub_head=new TreeNode(value);
-				 return sub_head;
-			  }
-			  //smaller
-			  else if(value<sub_head.val){
-				  sub_head.left=Tree_add(sub_head.left,value);
-			  }
-			  else{
-				  sub_head.right=Tree_add(sub_head.right,value);
-			  }
-			  return null;
+		  public TreeNode create_tree(ListNode head,ListNode tail){
+			  if(head==tail)  return null;
+			  ListNode slow=head;
+			  ListNode fast=head;
+			  
+			  while(fast!=tail&&fast.next!=tail){
+				  fast=fast.next.next;
+				  slow=slow.next;
+			  }//while
+			  TreeNode t_head=new TreeNode(slow.val);
+			  t_head.left=create_tree(head,slow);
+			  t_head.right=create_tree(slow.next,tail);
+			  return t_head;
 		  }
 		  
 		  public void tree_print(TreeNode head){
@@ -67,12 +53,7 @@ public class _109ConvertSortedListtoBinarySearchTree {
 		ListNode t6=new ListNode(6);
 		t1.next=t2;t2.next=t3;t3.next=t4;t4.next=t5;t5.next=t6;
 		TreeNode t_head=A.sortedListToBST(t1);
-		System.out.println("heihei");
 		A.tree_print(t_head);
-		while(t_head!=null){
-			System.out.print(t_head.val+" ");
-			t_head=t_head.right;
-		}
 		
 	}
 
